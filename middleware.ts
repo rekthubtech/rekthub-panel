6edaa76ee5944fce5b3f620a92d7c0486e1e82fb
rekthubtech <rekthubtech@gmail.com>
2026-06-26 16:09:40 +0300

    Create middleware.ts

---
+++ middleware.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export function middleware(req: NextRequest) {
+  const token = req.cookies.get('token')?.value
+  const { pathname } = req.nextUrl
+
+  if (pathname === '/login') {
+    if (token) return NextResponse.redirect(new URL('/channels', req.url))
+    return NextResponse.next()
+  }
+
+  if (!token) {
+    return NextResponse.redirect(new URL('/login', req.url))
+  }
+
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
+}
